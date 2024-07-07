291cfc7bc127edf7cdca04f9e10c6ae30770e4d3
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-07-07 19:57:47 +0400

    feat: implement sticky navigation bar

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 "use strict";
 
 // =========== Select HTML Elements ===========
+
 const navEl = document.querySelector(".nav");
 const navLinks = document.querySelector(".nav__links");
-
-// =========== Sticky Navigation ===========
-const currentScrollX = window.scrollX;
-const currentScrollY = window.scrollY;
-
-if (currentScrollY > 20) {
-  navEl.classList.add("sticky-nav");
-}
 
 // =========== Smooth Scrolling ===========
 
@@ -34,3 +27,34 @@ navLinks.addEventListener("click", function (e) {
     }
   }
 });
+
+// =========== Sticky Navigation ===========
+
+// window.addEventListener("scroll", function () {
+//   if (this.window.scrollY > 100) {
+//     navEl.classList.add("sticky-nav");
+//   } else {
+//     navEl.classList.remove("sticky-nav");
+//   }
+// });
+
+const summary = document.querySelector(".section--summary");
+const navHeight = navEl.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) {
+    navEl.classList.add("sticky-nav");
+  } else {
+    navEl.classList.remove("sticky-nav");
+  }
+};
+
+const summaryObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+summaryObserver.observe(summary);
