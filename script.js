47ef02c2b8d26b75282f73159ccd73530cec06ec
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-07-09 19:48:23 +0400

    feat: implement burger menu and navigation bar for small screan devices

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
 
 // =========== Select HTML Elements ===========
-
+const body = document.querySelector("body");
 const navEl = document.querySelector(".nav");
 const navLinks = document.querySelector(".nav__links");
+const burgerButton = document.querySelector(".burger-menu");
+
+// Navigation for burger menu
+const navBar = document.querySelector(".nav-bar");
+const navBarItems = document.querySelectorAll(".nav-bar__item");
 
 // =========== Smooth Scrolling ===========
 
@@ -30,11 +35,12 @@ navLinks.addEventListener("click", function (e) {
 
 // =========== Sticky Navigation ===========
 
-const cvContainer = document.querySelector(".cv-wrapper");
+const summarySec = document.querySelector(".section--summary");
+const navHeight = navEl.getBoundingClientRect().height;
 const stickyNav = function (entries) {
   const [entry] = entries;
 
-  if (entry.isIntersecting) {
+  if (!entry.isIntersecting) {
     navEl.classList.add("sticky-nav");
   } else {
     navEl.classList.remove("sticky-nav");
@@ -43,10 +49,11 @@ const stickyNav = function (entries) {
 
 const summaryObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0.1,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
 
-summaryObserver.observe(cvContainer);
+summaryObserver.observe(summarySec);
 
 // =========== Reveal Sections ===========
 const allRevealSections = document.querySelectorAll(".section--reveal");
@@ -69,4 +76,26 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allRevealSections.forEach((section) => {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
+});
+
+// =========== Burger Menu ===========
+burgerButton.addEventListener("click", function () {
+  if (!burgerButton.classList.contains("burger-menu--active")) {
+    burgerButton.classList.add("burger-menu--active");
+    navBar.classList.add("nav-bar--active");
+    body.classList.add("disable-scroll");
+  } else {
+    burgerButton.classList.remove("burger-menu--active");
+    navBar.classList.remove("nav-bar--active");
+    body.classList.remove("disable-scroll");
+  }
+});
+
+// Hide nav-bar when nav-bar item is clicked
+navBarItems.forEach((el) => {
+  el.addEventListener("click", function () {
+    navBar.classList.remove("nav-bar--active");
+    burgerButton.classList.remove("burger-menu--active");
+    body.classList.remove("disable-scroll");
+  });
 });
